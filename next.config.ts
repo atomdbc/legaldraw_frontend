bc085f4ee2beb9794f7ae9ea0ac5b0635e10583c
samuel lemikan <nexigen0@gmail.com>
2025-02-02 10:13:11 +0100

    added new

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Disable ESLint during production builds
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Disable TypeScript during production builds
     ignoreBuildErrors: true,
+  },
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ],
+      }
+    ]
   }
 };
 
